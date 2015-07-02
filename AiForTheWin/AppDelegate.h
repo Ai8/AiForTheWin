46aa7c3f5e99eab45c456c87c211b34ede63e4b4
codeMatt7 <matthew.houston7@yahoo.com>
2015-07-02 14:41:58 -0700

    Initial Commit

---
+++ AiForTheWin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AiForTheWin
+//
+//  Created by Matt Houston on 7/2/15.
+//  Copyright (c) 2015 AiForTheWin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
